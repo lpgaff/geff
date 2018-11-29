1e934575c3774d7666ea7f2fa7d07eb3d8984648
Liam Gaffney <lpgaff@gmail.com>
2018-11-29 19:39:44 +0100

    README updates

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # geff
+
+The efficiency curve is determined by a simultaneous fit to all
+experimental data. In the case that no normalisation data are
+given at all, then it is assumed to be equal to 1 for the first
+source and the other sources are fitted to this. In case multiple
+normalisations are given for a source, the global fit will minimise
+using all these values plus the constraints from the efficiency
+data of the other sources and their experimental normalisations.
+If source data given under efficiency doesn't have experimental
+values of normalisation constants, it's sufficient to simply type
+a dummy filename, i.e it doesn't have to exist. However, the
+ordering of the sources under -n must match those under -e.
+
+
+Usage: 
+'''
+geff -e <eff1.dat> -n <norm1.dat> ... -e <effX.dat> - n<normX.dat>
+'''
+
+effX.dat is the file containing the energy and efficiency
+values for source number X. The format is 4 columns:
+'''
+   Energy (keV) | Error (keV) | Efficiency (arb.) | Error (arb.)
+'''
+
+normX.dat is the file containing experimentally determined
+normalisation constants from arbitrary units to absolute %
+for source number X. The format is 2 columns:
+'''
+   Normalisation (%/arb.) | Error (%/arb.)
+'''
+'''
+geff --help
+'''
--- geff.cc
+++ geff.cc
@@ -23,26 +23,26 @@ void PrintUsage( char* progname ) {
 	
 	cout << "\nUsage: \n" << progname;
 	cout << " -e <eff1.dat> -n <norm1.dat> ... -e <effX.dat> - n<normX.dat>\n";
-	cout << "\neffX.dat is the file containing the energy and efficiency\n";
-	cout << "values for source number X. The format is 4 colums:\n";
+	cout << "\n effX.dat is the file containing the energy and efficiency\n";
+	cout << " values for source number X. The format is 4 columns:\n";
 	cout << "  Energy (keV) | Error (keV) | Efficiency (arb.) | Error (arb.)\n";
-	cout << "normX.dat is the file containing experimentally determined\n";
-	cout << "normalisation constants from arbitrary units to absolute %\n";
-	cout << "for source number X. The format is 2 columns:\n";
+	cout << "\n normX.dat is the file containing experimentally determined\n";
+	cout << " normalisation constants from arbitrary units to absolute %\n";
+	cout << " for source number X. The format is 2 columns:\n";
 	cout << "  Normalisation (%/arb.) | Error (%/arb.)\n\n";
-	cout << "The efficiency curve is determined by a simultaneous fit to all\n";
-	cout << "experimental data. In the case that no normalisation data are\n";
-	cout << "given at all, then it is assumed to be equal to 1 for the first\n";
-	cout << "source and the other sources are fitted to this. In case multiple\n";
-	cout << "normalisations are given for a source, the global fit will minimise\n";
-	cout << "using all these values plus the constraints from the efficiency\n";
-	cout << "data of the other sources and their experimental normalisations.\n";
-	cout << "If source data given under -e doesn't have experimental values\n";
-	cout << "of normalisation constants, it's sufficient to simply type a\n";
-	cout << "dummy filename, i.e it doesn't have to exist. However, the\n";
-	cout << "ordering of the sources under -n must match those under -e.\n";
+	cout << " The efficiency curve is determined by a simultaneous fit to all\n";
+	cout << " experimental data. In the case that no normalisation data are\n";
+	cout << " given at all, then it is assumed to be equal to 1 for the first\n";
+	cout << " source and the other sources are fitted to this. In case multiple\n";
+	cout << " normalisations are given for a source, the global fit will minimise\n";
+	cout << " using all these values plus the constraints from the efficiency\n";
+	cout << " data of the other sources and their experimental normalisations.\n";
+	cout << " If source data given under -e doesn't have experimental values\n";
+	cout << " of normalisation constants, it's sufficient to simply type a\n";
+	cout << " dummy filename, i.e it doesn't have to exist. However, the\n";
+	cout << " ordering of the sources under -n must match those under -e.\n";
 	
-	cout << "\n" << progname << " --help\tfor help!";
+	cout << "\n" << progname << " --help\tfor help!\n\n\n";
 	
 	return;
 	
